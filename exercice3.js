ff8d5e0b47767f32eebdca6da1dc77bf255259cc
AndreVictor60 <andre.victor60@free.fr>
2017-10-24 09:29:34 +0200

    Exercice 3 JavaScript

---
+++ exercice3.js
@@ -0,0 +1,9 @@
+// Déclarer les variables
+var hello ="Bonjour, ", name;
+/* Prompt = Boite de dialogue avec un champ où utilisateur peut saisir
+   Name = variable qui récuperer ce que utilisateur va fournir a prompt */
+name = prompt('Quel est votre prénom ?');
+// Concaténation des variables
+result = hello + name;
+// Afficher le résultat
+alert(result);
